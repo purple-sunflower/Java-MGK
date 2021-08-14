957ed612d6525d00f61d8c0ae8349736027b57a5
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2021-08-14 10:06:41 +0900

    Add files via upload

---
+++ secondmethod.java
@@ -0,0 +1,21 @@
+
+public class secondmethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 30;
+		int b = 10;
+		String result = "°á°ú";
+		
+		System.out.println("--------------------------------");
+		System.out.print("µ¡¼À, ");
+		System.out.print("»¬¼À, ");
+		System.out.print("°ö¼ÀÀÇ ");
+		System.out.printf("%s¸¦ º¼±î¿ä?\n", result);
+		System.out.printf("a+b ´Â %d, ", a+b);
+		System.out.printf("a-b ´Â %d, ", a-b);
+		System.out.printf("a*b ´Â %d\n", a*b);
+		System.out.println("--------------------------------");
+	}
+
+}
