c4ab90116773fa3a0137b8dcd3386ef4200cd866
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2021-08-14 10:07:00 +0900

    Add files via upload

---
+++ August12th.java
@@ -0,0 +1,20 @@
+
+public class August12th {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		float b = 0.15234f;
+		char c = 'a';
+		String d = "ÄÚ¹Â´ÏÆ¼";
+		boolean e = false;
+		double f = 3.141592653589793;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+	}
+
+}
