f1303789ecbea342f8b11538a96cb7b6979e4b3b
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2021-08-27 10:37:04 +0900

    Add files via upload

---
+++ twelveththread.java
@@ -0,0 +1,28 @@
+
+class NumberThread extends Thread {
+	public void run() {
+		for (int i = 0 ; i < 50 ; i++) {
+			System.out.print(i);
+		}
+	}
+}
+
+class CharThread extends Thread {
+	public void run() {
+		for (char i = 'a' ; i < 'z' ; i++) {
+			System.out.print(i);
+		}
+	}
+}
+
+public class twelveththread {
+	public static void main(String[] args) {
+		Thread thread1 = new NumberThread();
+		Thread thread2 = new CharThread();
+		
+		thread1.start();
+		thread2.start();
+	}
+}
+
+
